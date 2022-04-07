58e6c34fd672ddbed2ec3e509c68efe9bd5b742b
Tomasz-Marchlewski <tomasz.marchlewski@iqvia.com>
2022-04-07 10:00:59 +0200

    allowed users database table

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,7 @@ import logging
 import psycopg2
 
 import discord
+from cachetools.func import ttl_cache
 
 from src.settings import MY_ID, BOT_CALL, SECRET, DB_URL
 from .plugins.base import MyClientPlugin
@@ -17,6 +18,17 @@ class MyClient(discord.Client):
         self.db = psycopg2.connect(DB_URL)
         self.__plugins = []
 
+        # initialize client-wide tables
+        with self.db.cursor() as cur:
+            cur.execute("""
+                CREATE TABLE IF NOT EXISTS allowed_users (
+                    id SERIAL PRIMARY KEY,
+                    created_at TIMESTAMP DEFAULT now(),
+                    user_id BIGINT NOT NULL
+                );
+            """)
+        self.db.commit()
+
         for p in MyClientPlugin.__subclasses__():
             logger.info(f"register: {p.name}")
             self.__plugins.append(p(self))
@@ -30,9 +42,11 @@ class MyClient(discord.Client):
             await plugin.initialize_after_restart()
 
     async def on_message(self, message):
-        if message.author.id != MY_ID:
-            return
         if not message.content.startswith(BOT_CALL):
+            return
+        print("author ID:", message.author.id)
+        print("allowed users:", self._get_allowed_users())
+        if not message.author.id in self._get_allowed_users():
             return
 
         await self.broadcast_message(message)
@@ -42,7 +56,7 @@ class MyClient(discord.Client):
             await p.process_message(message)
 
     async def on_raw_reaction_add(self, payload):
-        if payload.member.id != MY_ID:
+        if payload.member.id in self._get_allowed_users():
             return
 
         await self.broadcast_reaction_add(payload)
@@ -50,6 +64,16 @@ class MyClient(discord.Client):
     async def broadcast_reaction_add(self, reaction):
         for p in self.__plugins:
             await p.process_reaction(reaction)
+
+    @ttl_cache(ttl=300)
+    def _get_allowed_users(self):
+        with self.db.cursor() as cur:
+            cur.execute("""
+                SELECT user_id
+                FROM allowed_users;
+            """)
+            users = cur.fetchall()
+        return [x[0] for x in users]
 
 
 def start():
