641c3b2791bb043bec36a72bc80490e5fcdbbcf2
Tomasz-Marchlewski <tomasz.marchlewski@iqvia.com>
2022-04-07 10:00:33 +0200

    BOT_CALL as env var

--- src/settings.py
+++ src/settings.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-BOT_CALL = '!magiczny-bot'
+BOT_CALL = os.getenv('BOT_CALL')
 SLEEP_TIME = 5 #s
 MY_ID = int(os.getenv('MY_ID'))
 SECRET = os.getenv('SECRET')
